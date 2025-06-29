14664999908a3885b7b9deba6548b92ba5a32700
GyanPrakashTYL <gyan.prakash@univolenitsolutions.com>
2025-06-29 19:54:30 +0530

    Added Lazy Loading

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
-import Home from './routes/Home'
-import Register from './routes/Register'
-import Login from './routes/Login'
 import { Provider } from 'react-redux'
 import store from './app/store'
+import { lazy } from 'react'
+import { Suspense } from 'react'
+
+const Home = lazy(() => import('./routes/Home'))
+const Register = lazy(() => import('./routes/Register'))
+const Login = lazy(() => import('./routes/Login'))
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store} >
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>  
+        <Suspense>
+          <Routes fallback={<div className='w-full mt-20 text-4xl font-bold flex justify-center'>Loading...</div>}>
+            <Route path='/' element={<Home />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </Suspense>  
       </BrowserRouter>
     </Provider>
   </StrictMode>,
